59aec0bb143ce6126ef28c0ba93fa279a865c574
Luke <lukem95@gmail.com>
2014-10-31 11:37:31 +0000

    More output, correct address for StartCOIN

--- app.js
+++ app.js
@@ -4,14 +4,18 @@ var Address = startcore.Address;
 var networks = startcore.networks;
 var coinUtil = startcore.util;
 
-var seed = 'Your seed goes here';
+var seed = 'Seed goes here';
 
 var knownBytes = coinUtil.sha256(seed);
 var hkey = HierarchicalKey.seed(knownBytes, 'startcoin');
 
-var derived = hkey.derive('m/0\'/0/0');
+var derived = hkey.derive('m/0');
 var priv = new startcore.PrivateKey(networks.startcoin.privKeyVersion, derived.eckey.private, derived.eckey.compressed);
 var wif = priv.toString();
 
-console.log('Using seed: ' + seed);
-console.log('Private key in WIF: ' + wif);
+console.log('Passphrase:\t\t' + seed);
+console.log('Derived Private Key:\t' + derived.extendedPrivateKeyString());
+console.log('Private Key (WIF):\t' + wif)
+console.log('Derived Public Key:\t' + derived.extendedPublicKeyString());
+console.log('Public Key (Hex):\t' + startcore.buffertools.toHex(derived.eckey.public));
+console.log('Address:\t\t' + Address.fromPubKey(derived.eckey.public, 'startcoin'));
