0b37a7c898524f23e12c07ce4c98166d06660f95
Luke <lukem95@gmail.com>
2014-10-31 10:17:44 +0000

    Bugfixes for startcore

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var startcore = require('startcore');
-var HierarchicalKey = startcore.HierarchicalKey;
+var startcore = require('bitcore');
+var HierarchicalKey = startcore.BIP32;
 var Address = startcore.Address;
 var networks = startcore.networks;
 var coinUtil = startcore.util;
@@ -7,10 +7,10 @@ var coinUtil = startcore.util;
 var seed = 'Your seed goes here';
 
 var knownBytes = coinUtil.sha256(seed);
-var hkey = HierarchicalKey.seed(knownBytes);
+var hkey = HierarchicalKey.seed(knownBytes, 'startcoin');
 
 var derived = hkey.derive('m/0\'/0/0');
-var priv = new bitcore.PrivateKey(networks.livenet.privKeyVersion, derived.eckey.private, derived.eckey.compressed);
+var priv = new startcore.PrivateKey(networks.startcoin.privKeyVersion, derived.eckey.private, derived.eckey.compressed);
 var wif = priv.toString();
 
 console.log('Using seed: ' + seed);
